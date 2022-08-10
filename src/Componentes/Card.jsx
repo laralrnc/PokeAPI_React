036e347c7deea4ef9d01a3ae7ee3c9a784dd5590
laralrnc <laralourenco5966@gmail.com>
2022-08-10 16:55:08 -0300

    Ajuste de estilo

--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -9,7 +9,6 @@ const Card = ({ pokemon, loading,PokeInfo}) => {
                     return (
                         <>
                             <div className="card" key={item.id} onClick={()=>PokeInfo(item)}>
-                                <h2>{item.id}</h2>
                                 <img src={item.sprites.front_default} alt="" />
                                 <h2>{item.name}</h2>
                             </div>
