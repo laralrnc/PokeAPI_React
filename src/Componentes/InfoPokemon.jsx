b73fe69a64b493bc5d0941f25fde472509a4fc0f
laralrnc <laralourenco5966@gmail.com>
2022-08-15 21:13:38 -0300

    Mudança de imagem para o easter egg - está mudando os outros pokemons também

--- src/Componentes/InfoPokemon.jsx
+++ src/Componentes/InfoPokemon.jsx
@@ -1,65 +1,55 @@
-import React, { useState } from "react";
 import ProgressBar from "@ramonak/react-progress-bar";
+import React, { useState } from "react";
 
 const InfoPokemon = ({ data }) => {
-    const [contador, setContador] = useState(0);
+  const [contador, setContador] = useState(0);
 
-    function alterarImagem(pikachu) {//Recebemos dois valores por parâmetro.
-        document.getElementById(pikachu).src = 'https://i.imgur.com/BEdL5Xl.png';
-    }
+  const pokemonImage = contador >= 3 ? 'https://i.imgur.com/BEdL5Xl.png' : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`
 
-    return (
-        <>
-            {
-                (!data) ? "" : (
+  return (
+    <>
+      {
+        (!data) ? "" : (
+          <>
+            {data.name !== 'pikachu' ? (
+              <h1>{data.name}</h1>
+            ) : (
+
+              <button className="btn-hover"
+                onClick={() => setContador((c) => c + 1)}>
+
+                --aqui exibi a quantidade de cliques só pra verificar se está contando certinho--
+                Você clicou {contador} vezes</button>
+            )}
+            <img src={pokemonImage} alt="" id='pikachu' />
+            <div className="habilidades">
+              {
+                data.abilities.map(poke => {
+                  return (
                     <>
-                        {data.name !== 'pikachu' ? (
-                            <h1>{data.name}</h1>
-                        ) : (
-
-                            <button className="btn-hover"
-                                onClick={alterarImagem}
-
-                            >
-
-                                Você clicou  vezes</button>
-
-
-
-
-                        )}
-                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`} alt="" id='pikachu' />
-                        <div className="habilidades">
-                            {
-                                data.abilities.map(poke => {
-                                    return (
-                                        <>
-                                            <div className="grupo">
-                                                <h2>{poke.ability.name}</h2>
-                                            </div>
-                                        </>
-                                    )
-                                })
-                            }
-
-                        </div>
-                        <div className="status">
-
-                            {
-                                data.stats.map(poke => {
-                                    return (
-                                        <>
-                                            <ProgressBar className="containerP" completed={poke.base_stat} customLabel={poke.stat.name} />
-                                        </>
-                                    )
-                                })
-                            }
-                        </div>
+                      <div className="grupo">
+                        <h2>{poke.ability.name}</h2>
+                      </div>
                     </>
-                )
-            }
-
-        </>
-    )
+                  )
+                })
+              }
+            </div>
+            <div className="status">
+              {
+                data.stats.map(poke => {
+                  return (
+                    <>
+                      <ProgressBar className="containerP" completed={poke.base_stat} customLabel={poke.stat.name} />
+                    </>
+                  )
+                })
+              }
+            </div>
+          </>
+        )
+      }
+    </>
+  )
 }
 export default InfoPokemon;
